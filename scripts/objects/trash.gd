e4547a22236bfaa74eb09b23b258f152c5966060
NullBitShift <93940058+NullBitShift@users.noreply.github.com>
2026-09-17 20:28:24 -0500

    refactor(player): switch player/object interaction from CharacterBody2D collision to PickupArea node

--- scripts/objects/trash.gd
+++ scripts/objects/trash.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 signal trash_collected
 
-func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player"):
+func _on_area_entered(area: Area2D) -> void:
+	if area.is_in_group("player"):
 		trash_collected.emit()
 		queue_free()
